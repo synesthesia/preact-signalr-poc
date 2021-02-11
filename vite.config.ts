9c1f59442ab37cf977816ffe0462db943912fd3e
Julian Elve <julian.elve@gmail.com>
2021-02-11 14:03:45 +0000

    edit config to put assets in same dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: ''
+  },
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
+    
   },
+  
   plugins: [preactRefresh()]
 })
