5fa9499fe12b5368f13b8f67e092d360f0070568
Julian Elve <julian.elve@gmail.com>
2021-02-11 17:10:53 +0000

    Simple Signalr receiver

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,20 @@
 import { h, Fragment } from 'preact'
 import { Logo } from './logo'
+import * as signalR from "@microsoft/signalr";
 
 export function App() {
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl(import.meta.env.VITE_SIGNALR_INFORMATION)
+    .withAutomaticReconnect()
+    .build();
+
+  connection.on("statusChanged", ( message: string) => {
+    console.log(message);
+  });
+
+  connection.start().catch((err: any) => console.error(err));
+
+
   return (
     <>
       <Logo />
